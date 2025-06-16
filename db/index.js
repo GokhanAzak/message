6b05d7abe1c474070e327c44c20918e6df7524e9
gokhan azak <azakgokhan@gmail.com>
2025-06-16 12:12:33 +0300

    Message servisi: temel API ve socket yapısı eklendi

--- db/index.js
+++ db/index.js
@@ -1,19 +1,11 @@
 const { Pool } = require('pg');
-require('dotenv').config();
-
-const db = require('./db');
-
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
-
-pool.connect()
-  .then(() => console.log('📦 PostgreSQL bağlantısı başarılı.'))
-  .catch((err) => console.error('❌ PostgreSQL bağlantı hatası:', err));
 
 module.exports = pool;
